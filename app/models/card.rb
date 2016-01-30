bdbf70eec39a33cb193fc005c9308dd6be6a0601
lyteuf <lydia.pechnikova@gmail.com>
2016-01-30 20:04:10 +0300

    change not_equals

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,14 @@
 class Card < ActiveRecord::Base	
-  validates :review_date,  presence: true
-  validates :original_text, presence: true
-  VALID_REGEX = /\A[a-z]+\z/
-  validates :original_text, :translated_text, format: { with:  VALID_REGEX } 
-  validates :translated_text, presence: true, 
-  exclusion: { in: :original_text, message: "translated_text = original_text" }
+validates :review_date,  presence: true
+validates :original_text, presence: true
+VALID_REGEX = /\A[a-z]+\z/
+validates :original_text, :translated_text, format: { with:  VALID_REGEX } 
+validates :translated_text, presence: true 
+
+validate :text_fields_are_not_equal
+
+ def text_fields_are_not_equal
+   self.errors.add(:base, 'translated_text and original_text cannot be equal.') if self.translated_text == self.original_text
+ end
+
 end
-
-
