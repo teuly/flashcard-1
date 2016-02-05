db12b83a51de8b4c4d63dec57c3e21ed92f23e9d
lyteuf <lydia.pechnikova@gmail.com>
2016-02-05 22:32:07 +0300

    valid objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
 require 'open-uri'
 require 'nokogiri'
+
 Card.delete_all
 data = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
 cards = data.css(".article")
-cards.xpath('//tbody/tr').each do |card|
-translated_text = card.css('td[2]').text # td[2] => '.bigLettr'
-original_text = card.xpath('td[3]').text
+
+cards.css(".rowB", ".rowA").each do |card|
+translated_text = card.css('.bigLetter').text
+original_text = card.css('td[3]').text
 card = Card.create!(:original_text  => original_text, :translated_text => translated_text, :review_date => Date.today )
 end
+
