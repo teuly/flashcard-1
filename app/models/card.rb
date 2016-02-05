cf66beb92e3b0bd9a1b1fd015f076d47edfadf07
lyteuf <lydia.pechnikova@gmail.com>
2016-02-05 21:28:33 +0300

    first object in db  invalid

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,14 +1,18 @@
 class Card < ActiveRecord::Base	
-#validates :review_date,  presence: true
-#validates :original_text, presence: true
+validates :review_date,  presence: true
+validates :original_text, presence: true
+
+before_create do |card|
+  card.translated_text = card.translated_text.downcase
+  card.original_text = card.original_text.downcase
+end
+
 #VALID_REGEX = /\A[a-z]+\z/
 #validates :original_text, :translated_text, format: { with:  VALID_REGEX } 
-#validates :translated_text, presence: true 
+validates :translated_text, presence: true 
+validate :not_equal
 
-#validate :not_equal
-
- #def not_equal
-  # errors.add(:base, 'translated_text and original_text cannot be equal.') if translated_text == original_text
- #end
-
+def not_equal
+errors.add(:base, 'translated_text and original_text cannot be equal.') if translated_text == original_text
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'open-uri'
 require 'nokogiri'
 Card.delete_all
 data = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
 cards = data.css(".article")
-
-cards.xpath('//tbody//tr').each do |card|
-original_text = card.css('.bigLetter').text
-translated_text = card.css('td[3]').text
-card = Card.create!(:original_text => original_text, :translated_text => translated_text)
-#puts "original_text", original_text
-#puts "translated text", translated_text.text
+cards.xpath('//tbody/tr').each do |card|
+translated_text = card.css('td[2]').text # td[2] => '.bigLettr'
+original_text = card.xpath('td[3]').text
+card = Card.create!(:original_text  => original_text, :translated_text => translated_text, :review_date => Date.today )
 end
