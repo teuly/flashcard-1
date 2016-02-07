8fbcc6e00282c3bfea943d94c93663f8f7cb7a45
lyteuf <lydia.pechnikova@gmail.com>
2016-02-07 13:16:50 +0300

    pull

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,16 +1,15 @@
 class Card < ActiveRecord::Base	
 validates :review_date,  presence: true
 validates :original_text, presence: true
+validates :translated_text, presence: true 
+validate :not_equal
 
 before_create do |card|
   card.translated_text = card.translated_text.downcase
   card.original_text = card.original_text.downcase
 end
-
 #VALID_REGEX = /\A[a-z]+\z/
 #validates :original_text, :translated_text, format: { with:  VALID_REGEX } 
-validates :translated_text, presence: true 
-validate :not_equal
 
 def not_equal
 errors.add(:base, 'translated_text and original_text cannot be equal.') if translated_text == original_text
