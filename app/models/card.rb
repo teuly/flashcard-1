194a8f15322a65d9cb646d4599e3f86a17b5535b
lyteuf <lydia.pechnikova@gmail.com>
2016-02-07 14:45:55 +0300

    cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,12 +4,12 @@ validates :original_text, presence: true
 validates :translated_text, presence: true 
 validate :not_equal
 
-before_create do |card|
-  card.translated_text = card.translated_text.downcase
-  card.original_text = card.original_text.downcase
-end
-#VALID_REGEX = /\A[a-z]+\z/
-#validates :original_text, :translated_text, format: { with:  VALID_REGEX } 
+#before_create do |card|
+#  card.translated_text = card.translated_text.downcase
+#  card.original_text = card.original_text.downcase
+#end
+VALID_REGEX = /\A[a-z]+\z/
+validates :original_text, :translated_text, format: { with:  VALID_REGEX } 
 
 def not_equal
 errors.add(:base, 'translated_text and original_text cannot be equal.') if translated_text == original_text
